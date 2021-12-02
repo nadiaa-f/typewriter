62e2f5344a3fe62eba8e39dc5ace9a5160a93912
Nadia Fatah <nadi.fatah@gmail.com>
2021-12-02 14:54:23 +0000

    lint fixes

--- typewriter.js
+++ typewriter.js
@@ -6,6 +6,6 @@ let timer = 0;
 for (const char of sentence) { /*loop through each character*/
   setTimeout(() => {
     process.stdout.write(char); /*stdout avoids automatically adding an extra "newline character" at the end each time.*/
-  }, timer)
-  timer += 50 /*each letter delay its appearance by 50ms (or so) longer than the previous letter.*/
-};
+  }, timer);
+  timer += 50; /*each letter delay its appearance by 50ms (or so) longer than the previous letter.*/
+}
